cb3d3cc5c9301e9ccddb2fc36d0721fc9cec3aa6
Connor Vosberg <connorjv@gmail.com>
2023-06-12 14:02:09 -0700

    creates a component for Breeder, Cat, Dog, and Shelter

---
+++ src/Breeder.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Breeder() {
+
+    return(
+        <div>
+            'Breeder'
+        </div>
+    )
+}
+
+export default Breeder;
+
---
+++ src/Shelter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Shelter() {
+
+    return (
+        <div>
+            'shelter'
+        </div>
+    )
+}
+
+export default Shelter;
