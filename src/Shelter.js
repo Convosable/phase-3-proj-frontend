e4373343b484a0ebf283b8b1ee7e61ed03ea4094
Connor Vosberg <connorjv@gmail.com>
2023-06-13 10:38:21 -0700

    updates Shelter component to make a fetch request to the backend database and render all shelters to the DOM

--- src/Shelter.js
+++ src/Shelter.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Shelter() {
 
+    const [sheltersList, setSheltersList] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:9292/shelters")
+        .then((r) => r.json())
+        .then(shelters => setSheltersList(shelters))
+    }, []);
+
     return (
         <div>
-            'shelter'
+            {sheltersList.map((shelter) => 
+                <div>
+                    <h1>{shelter.name}</h1>
+                    <h3>About us: {shelter.bio}</h3>
+                    <h3>Located at: {shelter.location}</h3>
+                    <h4>Established: {shelter.established_date}</h4>
+                </div>
+            )}
         </div>
     )
 }
