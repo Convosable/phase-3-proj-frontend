898a34751be5360d6f040d56d3b2503973b832b9
Connor Vosberg <connorjv@gmail.com>
2023-06-13 10:32:35 -0700

    updates Breeder compinent to make a fetch request to the backend database and render all breeders to the DOM

--- src/Breeder.js
+++ src/Breeder.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Breeder() {
 
+    const [breedersList, setBreedersList] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:9292/breeders")
+        .then((r) => r.json())
+        .then((breeder) => {
+            console.log(breeder)
+            setBreedersList(breeder)
+        })
+    }, []);
+
+
     return(
         <div>
-            'Breeder'
+            {breedersList.map((breeder) => 
+                <div>
+                    <h1>{breeder.name}</h1>
+                    <h3>About us: {breeder.bio}</h3>
+                    <h3>Located at: {breeder.location}</h3>
+                    <h4>Established: {breeder.established_date}</h4>
+                </div>
+            )}
         </div>
     )
 }
